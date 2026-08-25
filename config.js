02ad8747eac604d78648877d8d2b110fb75db1a1
Janaelson S. <125422751+Lidermal@users.noreply.github.com>
2026-08-25 18:56:14 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// SUBSTITUA PELAS SUAS CHAVES DO SUPABASE E ASAAS
+const SUPABASE_URL = 'https://evwsxwkvtjgexhjwofxh.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2d3N4d2t2dGpnZXhoandvZnhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc2ODk0MDEsImV4cCI6MjEwMzI2NTQwMX0.oN_ATHMc7KBHC7NA7O35Q5nS3H4OxSIAXMXvE7xYXCA';
+const ASAAS_API_KEY = 'SUA-CHAVE-API-ASAAS'; // Cuidado: Em produção, use Backend Proxy!
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+let currentUser = null;
+let currentRole = null;
